6ff9e4260887f6a4caf81a08f4509f3ba8e25fcb
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2026-05-26 14:56:19 -0300

    feat: adiciona testes de contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,19 @@
+describe('Funcionalidade: Contato', () => {
+  
+  it('Deve preencher o formulário de contato com sucesso', () => {
+    
+  });
+
+  it('teste 2', () => {
+    
+  });
+
+  it('teste 3', () => {
+    
+  });
+
+  it('teste 4', () => {
+    
+  });
+
+})
