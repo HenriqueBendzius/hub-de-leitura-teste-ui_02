04dbf39f2191d737354c0e3238df24902e2fb6ae
Henrique Bendzius <vendas.henriquebolsas@gmail.com>
2026-05-26 16:05:09 -0300

    Automação de contato

--- cypress/e2e/contato.cy.js
+++ cypress/e2e/contato.cy.js
@@ -1,19 +1,62 @@
 describe('Funcionalidade: Contato', () => {
+
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  });
+
   
   it('Deve preencher o formulário de contato com sucesso', () => {
-    
+    // cy.visit('http://localhost:3000/index.html')
+    cy.get('#name').type('Henrique Silva')
+    cy.get('#email').type('Henrique@teste.com')
+    cy.get('#subject').type('Parceiras')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    // cy.contains('Contato enviado com sucesso').should('exist')
+
   });
 
-  it('teste 2', () => {
-    
+  it('Deve validar mensagem de erro sem preencher nome', () => {
+    // cy.visit('http://localhost:3000/index.html')
+    cy.get('#name').clear()
+    cy.get('#email').type('Henrique@teste.com')
+    cy.get('#subject').type('Parceiras')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    // cy.get('#alert-container').clear('contain', 'Por favor, preencha o campo Nome')
+
   });
 
-  it('teste 3', () => {
-    
+  it('Deve validar mensagem de erro sem preencher E-mail', () => {
+    // cy.visit('http://localhost:3000/index.html')
+    cy.get('#name').type('Henrique')
+    cy.get('#email').clear()
+    cy.get('#subject').type('Parceiras')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+
+
   });
 
-  it('teste 4', () => {
-    
+  it('Deve validar mensagem de erro sem preencher assunto ', () => {
+    // cy.visit('http://localhost:3000/index.html')
+    cy.get('#name').type('Henrique')
+    cy.get('#email').type('teste@teste')
+    // cy.get('#subject').type('Parceiras')
+    cy.get('#message').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+
   });
+
+  it('Deve validar mensagem de erro sem preencher  mensagem ', () => {
+    // cy.visit('http://localhost:3000/index.html')
+    cy.get('#name').type('Henrique')
+    cy.get('#email').type('teste@teste')
+    cy.get('#subject').type('Parceiras')
+    cy.get('#message').clear()
+    cy.get('#btn-submit').click()
+  });
+
 
 })
